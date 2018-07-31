9ea74f1868d3f2c98310049aeb045424e4e8da63
huang_jx <1519236577@qq.com>
2018-07-31 02:26:47 -0400

    all html have done

--- app/admin/views.py
+++ app/admin/views.py
@@ -89,3 +89,51 @@ def moviecol_list():
 @admin.route('/oplog/list/')
 def oplog_list():
     return render_template('admin/oplog_list.html')
+
+
+# 管理员日志列表
+@admin.route('/adminloginlog/list/')
+def adminloginlog_list():
+    return render_template('admin/adminloginlog_list.html')
+
+
+# 会员日志列表
+@admin.route('/userloginlog/list/')
+def userloginlog_list():
+    return render_template('admin/userloginlog_list.html')
+
+
+# 添加权限
+@admin.route('/auth/add/')
+def auth_add():
+    return render_template('admin/auth_add.html')
+
+
+# 权限列表
+@admin.route('/auth/list/')
+def auth_list():
+    return render_template('admin/auth_list.html')
+
+
+# 添加角色
+@admin.route('/role/add/')
+def role_add():
+    return render_template('admin/role_add.html')
+
+
+# 添加列表
+@admin.route('/role/list/')
+def role_list():
+    return render_template('admin/role_list.html')
+
+
+# 添加管理员
+@admin.route('/admin/add/')
+def admin_add():
+    return render_template('admin/admin_add.html')
+
+
+# 管理员列表
+@admin.route('/admin/list/')
+def admin_list():
+    return render_template('admin/admin_list.html')
